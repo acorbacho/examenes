6642c93adb89ad9779e8ee42fb137f99ab99a56b
acorbacho <aaroncorbachovieites@gmail.com>
2023-05-20 18:28:39 +0200

    dados promesas

--- DWEC/examen5/dados.js
+++ DWEC/examen5/dados.js
@@ -1,20 +1,45 @@
-// Función que devuelve un dado aleatorio entre 1 y 6
-const lanzarDado = () => Math.floor(Math.random() * 6) + 1;
-// Función que devuelve la suma de dos dados
-const jugarRonda = () => {
-  const dado1 = lanzarDado();
-  const dado2 = lanzarDado();
-  return dado1 + dado2
-};
-// Función que devuelve el número de la tirada con el número más bajo, en caso de empate devuelve el primero
+// Función que devuelve una promesa que resuelve con un dado aleatorio entre 1 y 6 después de un retraso aleatorio
+const lanzarDado = () => new Promise((resolve, reject) => {
+  const retraso = Math.random() * 5000; // retraso aleatorio de hasta 5 segundos
+  setTimeout(() => {
+    const dado = Math.floor(Math.random() * 6) + 1;
+    dado <= 6 ? resolve(dado) : reject('Error');
+  }, retraso);
+});
+
+// Función que devuelve una promesa que resuelve con la suma de dos dados después de un retraso aleatorio
+const jugarRonda = () => Promise.all([lanzarDado(), lanzarDado()])
+  .then(([dado1, dado2]) => {
+    const resultado = dado1 + dado2;
+    const retraso = Math.random() * 1000; // retraso aleatorio de hasta 1 segundo
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(resultado);
+      }, retraso);
+    });
+  })
+  .catch(error => console.log(error)); // Si hay un error, se vuelve a jugar la ronda
+
+// Función que devuelve una promesa que resuelve con el número de la tirada con el número más bajo, en caso de empate devuelve el primero
 const jugarJuego = (numRondas) => {
-  const tiradas = Array.from({ length: numRondas }, jugarRonda);
-  const min = Math.min(...tiradas)
-  const posMin = tiradas.indexOf(min) + 1
-  return posMin
+  const tiradasPromises = Array.from({ length: numRondas }, jugarRonda);
+  const tiempoTotal = tiradasPromises.reduce((tiempo, tiradaPromesa) => {
+    return tiempo.then((tiradas) => {
+      return tiradaPromesa.then(tirada => [...tiradas, tirada]);
+    });
+  }, Promise.resolve([]));
+  return tiempoTotal.then((tiradas) => {
+    const min = Math.min(...tiradas);
+    const posMin = tiradas.indexOf(min) + 1;
+    return posMin;
+  });
 };
+
 // Simulación de 5 jugadores
 const numJugadores = 5;
-const ganador = jugarJuego(numJugadores);
+const ganadorPromise = jugarJuego(numJugadores);
+
 // Mostrar resultado por consola
-console.log(`La tirada con el número más bajo ha sido la número ${ganador}`)
+ganadorPromise.then((ganador) => {
+  console.log(`La tirada con el número más bajo ha sido la número ${ganador}`);
+});
